ed15bc2e95960ac397c8a15fccb11a5c3977babc
BohemianHacks <jeff.blecha@gmail.com>
2013-12-02 16:12:50 -0600

    Update wav.cpp

--- wav.cpp
+++ wav.cpp
@@ -81,7 +81,8 @@ std::vector <int> wavReader::getSample(unsigned pos){
     std::vector <int> channels;
     wav.seekg(pos+dataStart, wav.beg);
     for (int i = 0; i < numChannels; i++){
-        int val = getBytes(wav,bitsPerSample/8);
+        int val = ~getBytes(wav,bitsPerSample/8);
+        val = val + 1;
         channels.push_back(val);
     }
     return channels;

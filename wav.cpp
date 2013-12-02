1a1179cf4d2854ba63fd6a9977635aa14f9dd5a5
BohemianHacks <jeff.blecha@gmail.com>
2013-12-02 15:54:40 -0600

    Update wav.cpp

--- wav.cpp
+++ wav.cpp
@@ -96,7 +96,7 @@ int main(int argc, char** argv){
     }else{
         wavReader rdr(argv[1]);
         if (rdr.good){
-            for (unsigned j = 0; j < subchunk2size; j++){
+            for (unsigned j = 0; j < rdr.subchunk2size; j++){
                 std::vector <int> channels = rdr.getSample(j);
                 for (size_t i = 0; i < channels.size(); i++){
                     std::cout << channels[i] << '\n';
